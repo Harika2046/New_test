f72afae44ccae97b700538db42ff39874bb1486f
Harika2046 <44464021+Harika2046@users.noreply.github.com>
2018-11-16 10:29:37 +0530

    Update hello.java

--- PROJECT/hello.java
+++ PROJECT/hello.java
@@ -1,7 +1,7 @@
  class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wonder Wonderful World");
+        System.out.println("Hello Wonder Hai... please change Wonderful World");
     }
 
 }
